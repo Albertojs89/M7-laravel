468937baad079b76c5b6505e3567c2f64a5968fc
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2025-05-07 15:45:28 +0000

    login y register con middleware

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PokemonController;
+use App\Http\Controllers\AuthController;
+use GuzzleHttp\Promise\Is;
+use App\Http\Middleware\IsAdminAuth;
+use App\Http\Middleware\IsUserAuth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -30,6 +34,28 @@ Route::delete('/pokemon/{id}', [PokemonController::class, 'destroy']);
 //Actualiza parcialmente un Pokemon
 Route::patch('/pokemon/{id}', [PokemonController::class, 'updatePartial']);
 
+//PUBLIC ROUTES
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('/pokemon', [PokemonController::class, 'index']);
+Route::get('/pokemon/{id}', [PokemonController::class, 'show']);
+
+
+
+//PROTECTED ROUTES
+Route::middleware([IsUserAuth::class])->group(function () {
+    Route::get('me', [AuthController::class, 'getUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/pokemon', [PokemonController::class, 'store']);
+});
+
+// //ADMIN ROUTES
+// Route::middleware(\App\Http\Middleware\IsAdmin::class)->group(function () {
+//     Route::post('/pokemon', [PokemonController::class, 'store']);
+//     Route::put('/pokemon/{id}', [PokemonController::class, 'update']);
+//     Route::delete('/pokemon/{id}', [PokemonController::class, 'destroy']);
+//     Route::patch('/pokemon/{id}', [PokemonController::class, 'updatePartial']);
+// });
 
 
 ?>
